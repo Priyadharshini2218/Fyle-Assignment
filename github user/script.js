2b49a22ab21e1bb39010559a8ded674ac3b5baab
Priya2218 <80043495+Priyadharshini2218@users.noreply.github.com>
2022-11-25 16:29:06 +0530

    Add files via upload

---
+++ github user/script.js
@@ -0,0 +1,26 @@
+var form=document.getElementById("myform")
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    var search = document.getElementById("search").value
+    
+    var originalName = search.split(' ').join('')
+
+    alert(originalName)
+
+    fetch("https://api.github.com/users/"+originalName)
+    .then((result) => result.json())
+    .then((data) => {
+        console.log(data)
+
+        document.getElementById("result").innerHTML = `
+          <a target ="_blank" href="https://www.github.com/${originalName}"> Click here to view the profile to list pages 
+          </a>
+
+
+          `
+    })
+
+} )
